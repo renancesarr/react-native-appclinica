7774ab901bd8e09a2d226d7d0960677e7efa34b6
Renan Rabelo <renan.cesar.r@gmail.com>
2021-03-06 01:26:15 -0300

    cards scroll viewr

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Container, Foto, TextTitulo, TextSubTitulo, Collum1, Collum2, Icon } from './style';
-import { Text } from 'react-native'
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Container, Foto, TextTitulo, TextSubTitulo, Collum1, Collum2, Icon, CardItem, Carousel, ContainerCarousel } from './style';
 
 
 const Home: React.FC = () => (
 
-  <Container>
-    <Collum1>
-      <Foto></Foto>
-      <TextTitulo>Seja bem vindo.</TextTitulo>
-      <TextSubTitulo>Agende sua consulta de forma rápida e segura.</TextSubTitulo>
-    </Collum1>
-    <Collum2>
-      <Icon name="bell" size={24} color="#fff"></Icon>
-    </Collum2>
+  <>
+    <Container>
+      <Collum1>
+        <Foto></Foto>
+        <TextTitulo>Seja bem vindo.</TextTitulo>
+        <TextSubTitulo>Agende sua consulta de forma rápida e segura.</TextSubTitulo>
+      </Collum1>
+      <Collum2>
+        <Icon name="bell" size={24} color="#fff"></Icon>
+      </Collum2>
+    </Container>
+    <ContainerCarousel>
+      <Carousel horizontal showsHorizontalScrollIndicator={false}>
+        <CardItem key='teste'><Text>asihasuhsaiuhsa</Text></CardItem>
+        <CardItem key='teste1'><Text>asihasuhsaiuhsa</Text></CardItem>
+        <CardItem key='teste2'><Text>asihasuhsaiuhsa</Text></CardItem>
+        <CardItem key='teste10'><Text>asihasuhsaiuhsa</Text></CardItem>
+        <CardItem key='teste11'><Text>asihasuhsaiuhsa</Text></CardItem>
+        <CardItem key='teste21'><Text>asihasuhsaiuhsa</Text></CardItem>
+      </Carousel>
+    </ContainerCarousel>
 
-  </Container>
+
+  </>
 );
 
 export default Home;
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -7,6 +7,7 @@ export const Container = styled.View`
   max-height: 50%;
   border-bottom-left-radius: 40px;
   border-bottom-right-radius: 40px;
+  box-shadow: 2px 2px 3px grey;
   align-content:center;
   flex:1;
   flex-direction: row;
@@ -45,5 +46,22 @@ export const TextSubTitulo = styled.Text`
 `
 export const Icon = styled(FeatherIcon)`
   padding-right:24px;
+`;
+export const CardItem = styled.View`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  margin-left: 16px;
+  background-color: #E9EFFE;
+  max-width: 128px;
+  max-height: 128px;
+  border-radius: 20px;
+  box-shadow: 1px 1px 1px black;
+`;
+export const ContainerCarousel = styled.View`
+  flex: 1;
+`;
 
+export const Carousel = styled.ScrollView`
+  max-height: 150px;
+  flex: 1;
 `;
